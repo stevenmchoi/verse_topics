9ae1739e6ed984b2f80f861091e29eab8a2de119
Steven Choi <steven.m.choi@gmail.com>
2018-10-04 18:21:08 -0700

    Create about page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1c5e4a92b494037d2c44";
+/******/ 	var hotCurrentHash = "f6406d7a7ac19bc4340e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
